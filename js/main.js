198d6853d6180206307ae9d36cdb4c16772484f0
ValentinaRazinova <120265376+ValentinaRazinova@users.noreply.github.com>
2022-12-11 02:18:49 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// Carousel
+const owl = $('.owl-carousel');
+
+owl.owlCarousel({
+	center: true,
+	loop: true,
+	margin: 20,
+	startPosition: 0,
+	items: 1,
+	responsive: {
+		540: {
+			items: 3,
+			startPosition: 1,
+		},
+		1200: {
+			items: 3,
+			margin: 30,
+		},
+	},
+});
+
+$('.slider__btn--prev').click(function () {
+	owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn--next').click(function () {
+	owl.trigger('next.owl.carousel');
+});
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
+
+let buttons;
+buttons = document.getElementsByClassName("btn btn--subscribe");
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].onclick = function () {
+		document.getElementById("contacts").scrollIntoView({behavior: "smooth"});
+	}
+}
+
+document.getElementById("form").onclick = function () {
+	if (document.getElementById("email").value === "") {
+		alert("Заполните вашу почту!");
+	}  else {
+		alert("Спасибо, что подписались на обновления!");
+	}
+};
